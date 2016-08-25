a052baf880a3e1416179447dd01931361b1a6ad1
Rhen <rstorrejos@student.com>
2016-08-26 07:23:42 +0800

    Add all back office features And Validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,9 @@
 class Group < ApplicationRecord
+  validates :group_name, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :contact, presence: true
+  validates :location, presence: true
+
 
 end
