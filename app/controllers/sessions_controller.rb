f722d69396e1ff8ad545d5c45e2f060a26613a58
Rhendel Torrejos <rstorrejos@student.apc.edu.ph>
2016-08-25 23:58:56 -0700

    Paayos ng links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by_email(params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to '/'
+      redirect_to 'welcome/index'
     else
       redirect_to 'login'
     end
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/'
+    redirect_to '/welcome/index'
   end
 
 end
